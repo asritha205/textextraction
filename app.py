a5b3a5778b05220fd709266a7d8451f9e18ab07c
asritha706 A <asritha706@gmail.com>
2023-10-25 17:05:21 +0000

    2nd

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
-from flask import Flask,render_template,Response
-
-from Test import emotion
-
-detect = emotion()
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return render_template('index.html')
 
-def gen(detect):
-    
-    while True:
-        frame = detect.detect_emotion()
-        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        
+@app.route('/', methods=['GET'])
+def hello_word():
+    return render_template('index.html')
 
-@app.route('/predict')
-
+@app.route('/', methods=['POST'])
 def predict():
-    return Response(gen(detect),mimetype='multipart/x-mixed-replace;boundary=frame')
+    imagefile= request.files['imagefile']
+    image_path = "./images/" + imagefile.filename
+    imagefile.save(image_path)
+
+    image = load_img(image_path, target_size=(224, 224))
+    image = img_to_array(image)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    image = preprocess_input(image)
+    yhat = model.predict(image)
+    label = decode_predictions(yhat)
+    label = label[0][0]
+
+    classification = '%s (%.2f%%)' % (label[1], label[2]*100)
+
+
+    return render_template('index.html', prediction=classification)
+
+
 if __name__ == '__main__':
-   app.run()
+    app.run(port=3000, debug=True)
